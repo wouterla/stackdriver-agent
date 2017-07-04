82ba11eb214b22349d8beb52b30855fbc4dafd38
Wouter Lagerweij <wouter@lagerweij.com>
2017-07-04 12:49:36 +0200

    Working version, but... hack! just logging journalctl to stdout

--- run-agent.sh
+++ run-agent.sh
@@ -12,46 +12,24 @@ set -eo pipefail
 
 for f in /opt/configurator/*.sh; do source $f; done
 
-# if curl metadata.google.internal -i > /dev/null 2>&1; then
-#   /opt/stackdriver/stack-config --write-gcm --no-start
-# else
-#   if [ -z "${STACKDRIVER_API_KEY}" ]; then
-#     echo "Need to specify STACKDRIVER_API_KEY as an environment variable."
-#     exit 1
-#   else
-#     /opt/stackdriver/stack-config --api-key "${STACKDRIVER_API_KEY}" --no-start
-#   fi
-# fi
-#
-# stopDaemon() {
-#   service stackdriver-agent stop
-#   kill -9 ${LOG_PID}
-# }
-
 stopFluentd() {
   service google-fluentd stop
   kill -9 ${FLUENTD_LOG_PID}
 }
 
-
-# service stackdriver-agent start
-# trap stopDaemon SIGINT SIGTERM
-# touch /var/log/collectd.log
-# tail /var/log/collectd.log -f &
-# LOG_PID=$!
-
 service google-fluentd start
 trap stopFluentd SIGINT SIGTERM
-touch /var/log/google-fluentd/google-fluentd.log
-tail /var/log/google-fluentd/google-fluentd.log -f &
-FLUENTD_LOG_PID=$!
+#touch /var/log/google-fluentd/google-fluentd.log
+#tail /var/log/google-fluentd/google-fluentd.log -f &
+# Send all journalctl data to stdout as long as fluentd isn't working
+journalctl -f | grep -v "docker\["
 
-# echo Waiting for fluentd daemon to start
-while [ ! -f "${FLUENTD_PID_FILE}" ] ; do
-  sleep 1;
-done
-
-# Waiting for fluentd daemon to stop
-while [ -f "${FLUENTD_PID_FILE}" ] ; do
-  sleep 1;
-done
+# # echo Waiting for fluentd daemon to start
+# while [ ! -f "${FLUENTD_PID_FILE}" ] ; do
+#   sleep 1;
+# done
+#
+# # Waiting for fluentd daemon to stop
+# while [ -f "${FLUENTD_PID_FILE}" ] ; do
+#   sleep 1;
+# done
